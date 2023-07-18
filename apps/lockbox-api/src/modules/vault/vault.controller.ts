3198bc8f711c1dd6320b83af6374b2dd32ae00ef
janani-maya <janimaya13@gmail.com>
2023-07-18 15:38:08 +0530

    feat(vault): LOC-05 create user's password vault (BE)

---
+++ apps/lockbox-api/src/modules/vault/vault.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { VaultService } from './vault.service';
+import { AuthGuard } from '@nestjs/passport';
+import { Vault } from './schemas/vault.schema';
+import { CreateVaultDto } from './dto/create-vault.dto';
+import { getCurrentUserId } from '../auth/decorators/currentUserId';
+import { UpdateVaultDto } from './dto/update-vault.dto';
+import { deleteVaultDto } from './dto/delete-vault.dto';
+
+@Controller('vault')
+export class VaultController {
+  constructor(private vaultService: VaultService) {}
+
+  //create a 1 vault in the vaults array of user
+  @Post()
+  @UseGuards(AuthGuard())
+  async createVault(
+    @Body() vault: CreateVaultDto,
+    @getCurrentUserId() userId: string,
+  ): Promise<Vault> {
+    console.log('req.user', userId);
+    return this.vaultService.createVault(vault, userId);
+  }
+}
