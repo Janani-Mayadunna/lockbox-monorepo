8217959627148b2b33b6f87e90cb781f99a35404
janani-maya <janimaya13@gmail.com>
2023-07-18 15:54:46 +0530

    feat(vault): LOC-7 update and delete password vaults (BE)

--- apps/lockbox-api/src/modules/vault/vault.service.ts
+++ apps/lockbox-api/src/modules/vault/vault.service.ts
@@ -50,4 +50,86 @@ export class VaultService {
 
     return vaults;
   }
+
+  async getOneUserVault(userId: string, vaultId: string): Promise<Vault> {
+    const user = await this.userModel.findById(userId);
+
+    if (!user) {
+      logger.error(`User not found with id: ${userId}`);
+      return null;
+    }
+
+    if (user.vaults.length === 0) {
+      logger.error(`User has no vaults`);
+      throw new NotFoundException(`User has no vaults`);
+    }
+
+    // Retrieve the vault for the user
+    const vault = await this.vaultModel.findOne({
+      user: user._id,
+      _id: vaultId,
+    });
+
+    return vault;
+  }
+
+  async updateOneUserVault(
+    userId: string,
+    vaultId: string,
+    UpdateVaultData: IUpdateVault,
+  ): Promise<Vault> {
+    const user = await this.userModel.findById(userId);
+
+    if (!user) {
+      logger.error(`User not found with id: ${userId}`);
+      return null;
+    }
+
+    if (user.vaults.length === 0) {
+      logger.error(`User has no vaults`);
+      throw new NotFoundException(`User has no vaults`);
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(vaultId)) {
+      logger.error(`Invalid vault id: ${vaultId}`);
+      throw new NotFoundException(`Invalid vault id: ${vaultId}`);
+    }
+
+    //if vaultId is not in user's vaults array, throw error
+
+    // Retrieve the vault for the user
+    const updatedVault = await this.vaultModel.findOneAndUpdate(
+      {
+        user: user._id,
+        _id: vaultId,
+      },
+      UpdateVaultData,
+      { new: true },
+    );
+
+    return updatedVault;
+  }
+
+  async deleteOneUserVault(
+    userId: string,
+    deleteVaultData: IDeleteVault,
+  ): Promise<Vault> {
+    //remove that from element from user's vaults array
+    await this.userModel.updateOne(
+      { _id: userId },
+      { $pull: { vaults: deleteVaultData.id } },
+    );
+
+    // Retrieve the vault for the user
+    const deletedVault = await this.vaultModel.findOneAndDelete({
+      user: userId,
+      _id: deleteVaultData.id,
+    });
+
+    // //remove that from element from user's vaults array
+    // user.vaults.pull(deletedVault._id);
+    // await user.save();
+
+    return deletedVault;
+  }
 }
