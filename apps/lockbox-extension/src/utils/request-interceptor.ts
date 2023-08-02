3eb5d53d864cfd5417b50ede2220e7d809d64193
janani-maya <janimaya13@gmail.com>
2023-08-02 10:23:58 +0530

    feat(extension): Implement login functionality

---
+++ apps/lockbox-extension/src/utils/request-interceptor.ts
@@ -0,0 +1,38 @@
+function authInterceptor(request: Request): Request {
+  const tokenString = localStorage.getItem('jwt-lockbox');
+  const userToken = JSON.parse(tokenString!);
+  const access_token = userToken.access_token;
+
+  if (access_token) {
+    request.headers.set('Authorization', `Bearer ${access_token}`);
+  }
+  return request;
+}
+
+export function authorizedFetch(
+  url: string,
+  options: RequestInit
+): Promise<Response> {
+  const request = new Request(url, options);
+  const interceptedRequest = authInterceptor(request);
+  return fetch(interceptedRequest);
+}
+
+export function getVaultKey(): string {
+  const storedVaultKey = localStorage.getItem('VK');
+  const vaultKey = storedVaultKey!;
+  return vaultKey;
+}
+
+export function getUserSalt(): string {
+  const storedVaultKey = localStorage.getItem('current-user');
+  const userData = JSON.parse(storedVaultKey!);
+  const salt = userData.salt;
+  return salt;
+}
+
+export function getLoggedIn(): boolean {
+  const isLoggedIn = localStorage.getItem('isLoggedIn');
+  const loggedIn = isLoggedIn === 'true' ? true : false;
+  return loggedIn;
+}
